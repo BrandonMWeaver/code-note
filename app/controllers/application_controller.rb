abce109d4e7f6eed5d4376221f670fc197d20751
Brandon Weaver <a7xncob@gmail.com>
2020-02-07 18:30:52 -0500

    added basic helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+	helper_method :logged_in?, :current_user
+	
+	private
+
+	def logged_in?
+		return !!session[:user_id]
+	end
+
+	def current_user
+		return @current_user ||= User.find_by_id(session[:user_id])
+	end
+
 end
