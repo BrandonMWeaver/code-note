efaab65e578781098fe811ab1d6839d9f1c1538e
Brandon Weaver <a7xncob@gmail.com>
2020-02-08 12:20:08 -0500

    fixed Post validations/added basic styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 
 	validates :title, presence: true
-	validates :content, length { minimum: 10 }
-	validates :description, length { minimum: 10 }
+	validates :content, length: { minimum: 10 }
+	validates :description, length: { minimum: 10 }
 
 end
