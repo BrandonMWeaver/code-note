563e2db97f7734ad134d98437eaec43c7bc57879
Brandon Weaver <a7xncob@gmail.com>
2020-02-08 14:08:14 -0500

    added sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-
+		@user = User.find_by(username: params[:user][:username])
+		if @user.authenticate(params[:user][:password])
+			session[:user_id] = @user.id
+			redirect_to user_path(@user.slug)
+		else
+			render :new
+		end
 	end
 
 	def destroy
-
+		session.delete :user_id
+		redirect_to root_path
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "static#home"
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+  get "/signout", to: "sessions#destroy"
   resources :posts
   resources :languages
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy], param: :slug
