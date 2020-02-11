74b4f941b8e9928ff955b23d387a5569dc8debda
Brandon Weaver <a7xncob@gmail.com>
2020-02-10 19:05:35 -0500

    added basic routes and controller actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+
+	def index
+		if params[:slug].present?
+			@user = User.find_by_slug(params[:slug])
+			@posts = @user.posts
+		else
+			@posts = Post.all
+		end
+	end
+
+	def new
+		@post = Post.new(user_id: User.find_by_slug(params[:slug]).id)
+		unless logged_in? && @post.user == current_user
+			redirect_to users_posts_path(@post.user.slug)
+		end
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.user = current_user
+		if @post.save
+			redirect_to users_posts_path(@post.user.slug)
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def post_params
+		return params.require(:post).permit(:title, :content, :description, :user_id, :language_id)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
   get "/signout", to: "sessions#destroy"
-  resources :posts
-  resources :languages
+
   get "/users/new", to: "users#new", as: "new_user"
   post "/users/new", to: "users#create"
   get "/users/:slug", to: "users#show", as: "user"
   get "/users/:slug/edit", to: "users#edit", as: "edit_user"
   patch "/users/:slug/edit", to: "users#update"
   put "/users/:slug/edit", to: "users#update"
+
+  get "/users/:slug/posts", to: "posts#index", as: "users_posts"
+  get "/users/:slug/posts/new", to: "posts#new", as: "new_users_post"
+  post "/users/:slug/posts/new", to: "posts#create"
 end
