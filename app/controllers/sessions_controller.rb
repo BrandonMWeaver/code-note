e32afa6bf750fc1483b3bf706148a5675bf08ed5
Brandon Weaver <a7xncob@gmail.com>
2020-02-13 20:01:55 -0500

    improved error handling for omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,16 @@ class SessionsController < ApplicationController
 		if auth
 			@user = User.find_by(email: auth["info"]["email"])
 			unless @user
-				@user = User.create(username: auth["info"]["name"], email: auth["info"]["email"], password: SecureRandom.hex)
+				@user = User.new(username: auth["info"]["name"], email: auth["info"]["email"], password: SecureRandom.hex)
+				if @user.save
+					session[:user_id] = @user.id
+					redirect_to user_path(@user.slug)
+				else
+					render :new
+				end
+			else
+				session[:user_id] = @user.id
 			end
-			session[:user_id] = @user.id
-			redirect_to user_path(@user.slug)
 		else
 			@user = User.find_by(username: params[:user][:username])
 			if @user && @user.authenticate(params[:user][:password])
