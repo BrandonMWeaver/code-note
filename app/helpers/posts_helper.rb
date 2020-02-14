bff4e203be0a9c9b0800b120fff45507c369a2e3
Brandon Weaver <a7xncob@gmail.com>
2020-02-14 12:33:40 -0500

    moved a small amount of logic for the posts index view into a helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+
+	def conditional_link_to_for(post)
+		if logged_in? && post.user == current_user
+			link_to "Edit", edit_users_post_path(post.user.slug, post)
+		else
+			link_to post.user.username, user_path(post.user.slug)
+		end
+	end
+
 end
